f23e91bb793759d62de97a48290991cd6f95ca5f
topcheto356 <miro.kurtev27@gmail.com>
2022-08-17 17:22:43 +0300

    update navigation

--- src/gameOfLifeSolutions/solutionWithJSMap/components/Navigatiom.js
+++ src/gameOfLifeSolutions/solutionWithJSMap/components/Navigatiom.js
@@ -1,37 +1,22 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 const Naviagation = (props) => {
-	//show next generation button state
-	const [showSt, setShowSt] = useState(false);
-
 	//remove the for after the grid is selected
 	const onClickGridHandler = () => {
 		props.showForm(false);
-
-		//reset the game
-		setShowSt(false);
 		props.reset();
 	};
 
 	//render generation 0
-	const onClickSeedsHandler = () => {
-		props.loadGerneration0();
-		setShowSt(true);
-	};
-
-	//render next generation
-	const onClickStartHandler = () => {
-		props.start();
+	const onClickNext = () => {
+		props.loadNextGerneration();
 	};
 
 	return (
 		<nav className='navigation'>
 			<ul className='list'>
 				<li onClick={onClickGridHandler}>Create grid</li>
-				{props.showSe && (
-					<li onClick={onClickSeedsHandler}>Confirm selected seeds</li>
-				)}
-				{showSt && <li onClick={onClickStartHandler}>Next generation</li>}
+				{props.showNext && <li onClick={onClickNext}>Next generation</li>}
 			</ul>
 		</nav>
 	);
