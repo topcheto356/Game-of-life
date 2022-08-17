326621e75337f0b84fb8b7d56226905a0f27a923
topcheto356 <miro.kurtev27@gmail.com>
2022-08-17 17:23:28 +0300

    update rendering grid(unfinished)

--- src/gameOfLifeSolutions/solutionWithJSMap/components/Grid.js
+++ src/gameOfLifeSolutions/solutionWithJSMap/components/Grid.js
@@ -1,46 +1,18 @@
 import React from 'react';
 
 const Grid = (props) => {
-	//show selected seeds for generation 0
-	const onClickHandler = (event, key) => {
-		props.loadSeeds(key);
+	//select generation 0
+	const onClickHandler = (event, cords) => {
+		if (event.target.className === 'squere-dead') {
+			event.target.className = 'squere-alive';
+		} else {
+			event.target.className = 'squere-dead';
+		}
 
-		event.target.className =
-			event.target.className === 'squere-dead' ? 'squere-alive' : 'squere-dead';
+		props.selectGeneraion0(cords);
 	};
 
-	return (
-		<div className='grid'>
-			{props.grid.map((fel, i) => {
-				//only render cells in given grid
-				const outsideRow =
-					i >= props.bonus && i < props.grid.length - props.bonus;
-
-				return (
-					outsideRow && (
-						<div className='row' key={`row-${i}`}>
-							{fel.map((sel, j) => {
-								//only render cells in given grid
-								const outsideColumn =
-									j >= props.bonus && j < fel.length - props.bonus;
-
-								const className = sel ? 'squere-alive' : 'squere-dead';
-								return (
-									outsideColumn && (
-										<div
-											className={className}
-											key={`row-${i} column-${j}`}
-											onClick={(event) => onClickHandler(event, `${i},${j}`)}
-										></div>
-									)
-								);
-							})}
-						</div>
-					)
-				);
-			})}
-		</div>
-	);
+	return <div className='grid'>{}</div>;
 };
 
 export default Grid;
